1d07bc35ed4fd7b09d8ddac1d0617976546f8d9f
MernaaMourad <mernaaamouradd@gmail.com>
2025-07-08 15:41:16 +0200

    rest of product addition

--- js/collapse.js
+++ js/collapse.js
@@ -645,7 +645,7 @@ document.addEventListener("DOMContentLoaded", function() {
             // Find the <a> tag with class 'opensearch' inside the clone and replace its class
             const openSearchLink = clonedSection.querySelector('a.opensearch');
             if (openSearchLink) {
-                openSearchLink.classList.remove('opensearch'); 
+                openSearchLink.classList.remove('opensearch');
                 openSearchLink.classList.add('opensearch2');
             }
 
@@ -745,39 +745,40 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener("DOMContentLoaded", function() {
 
 
-  const coreTab = document.querySelector('.core-banking-tab');
-  const subTabs = document.getElementById('coreBankingSubtabs');
+    const coreTab = document.querySelector('.core-banking-tab');
+    const subTabs = document.getElementById('coreBankingSubtabs');
 
-  if(subTabs)
-  {
-    subTabs.style.display = "none";
-  }
- 
-
-  if (coreTab && subTabs) {
-    coreTab.addEventListener('click', function () {
-      subTabs.style.display = (subTabs.style.display === 'none' || subTabs.style.display === '') 
-        ? 'block' 
-        : 'none';
-    });
-  }
-
-
-  if (window.location.href.includes('/search-api')) {
-    const title = document.querySelector('#page-node-title');
-    if (title) {
-      title.style.paddingLeft = 'unset';
+    if (subTabs) {
+        subTabs.style.display = "none";
     }
- 
-    const header = document.querySelector('h1.js-quickedit-page-title.page-header');
-    if (header) {
-      header.style.backgroundColor = 'transparent';
-      header.style.paddingBottom = '3rem';
-      header.style.fontSize = '40px';
-    }
-  }
 
-    
+
+    if (coreTab && subTabs) {
+        coreTab.addEventListener('click', function() {
+            subTabs.style.display = (subTabs.style.display === 'none' || subTabs.style.display === '') ?
+                'block' :
+                'none';
+        });
+    }
+
+
+    if (window.location.href.includes('/search-api')) {
+        const title = document.querySelector('#page-node-title');
+        if (title) {
+            title.style.paddingLeft = 'unset';
+            title.style.width = '100%';
+        }
+
+
+        const header = document.querySelector('h1.js-quickedit-page-title.page-header');
+        if (header) {
+            header.style.backgroundColor = 'transparent';
+            header.style.paddingBottom = '3rem';
+            header.style.fontSize = '40px';
+        }
+    }
+
+
     function addBackgroundImages(selector) {
         const targetElement = document.querySelector(selector);
 
